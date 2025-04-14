7f810923ee29ae870576ca0cbf909e2abae1b3d9
Pawan Lekurwale <142106139+plpatil1@users.noreply.github.com>
2025-04-14 20:35:01 +0530

    Update pawan.sql

--- Day01/pawan.sql
+++ Day01/pawan.sql
@@ -2,3 +2,6 @@ select DISTINCT COUNT(product_id),discount_per from Productt group by discount_p
 
 
  select DISTINCT COUNT(product_id)as Count, sum(discount_per)as Total_Amount from Productt group by product_id;
+
+
+select sum(o.total_amount)AS Total_Amount, count(c.customer_id)AS COUNTofID from Orderr o join Customerr c using(customer_id) group by c.state;
